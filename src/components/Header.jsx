6f39c252a5c389ce5f29936085af689c55cbc94f
AnViKkFR7 <anvikedits@gmail.com>
2026-02-05 12:16:39 +0100

    añadir logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,9 @@ function Header() {
   return (
     <header className="header">
       <div className="container">
-        <Link to="/" className="logo">Moira Ordo</Link>
+        <Link to="/" className="logo">
+          <img src="/logo2.png" alt="Moira Ordo" className="logo-img" />
+        </Link>
         <nav className="nav">
           <Link to="/producto">Producto</Link>
           <Link to="/nosotros">Nosotros</Link>
